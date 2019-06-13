8d78e05eced4deb2a7a967c97360ca3c3b5cdb4e
arhh <aulee7@icloud.com>
2019-06-13 22:14:09 +0100

    Add barebones drag and drop functionality Limitations: Once list emptied, cannot refill it; Layout goes a bit mad when list items moved.

--- script.js
+++ script.js
@@ -2,40 +2,51 @@
     window.addEventListener("DOMContentLoaded", init, false);
 
     function init() {
-        console.log("script init'd");
+
+        var toDoList = document.querySelector("#to-do-list");
+        var doingList = document.querySelector("#doing-list");
+        var doneList = document.querySelector("#done-list");
 
         var toDoListItems = document.querySelectorAll("#to-do-list li");
         var doingListItems = document.querySelectorAll("#doing-list li");
         var doneListItems = document.querySelectorAll("#done-list li");
 
+        toDoList.addEventListener("drop", handleDrop, false);
+        toDoList.addEventListener("dragover", handleDragover, false);
+        doingList.addEventListener("drop", handleDrop, false);
+        doingList.addEventListener("dragover", handleDragover, false);
+        doneList.addEventListener("drop", handleDrop, false);
+        doneList.addEventListener("dragover", handleDragover, false);
+
         for (let i = 0; i < toDoListItems.length; i++) {
-            console.log(toDoListItems[i]);
+            // console.log(toDoListItems[i]);
             toDoListItems[i].addEventListener("dragstart", handleDragStart, false);
-            toDoListItems[i].addEventListener("dragend", handleDragEnd, false);
         }
 
         for (let i = 0; i < doingListItems.length; i++) {
-            console.log(doingListItems[i]);
+            // console.log(doingListItems[i]);
             doingListItems[i].addEventListener("dragstart", handleDragStart, false);
-            doingListItems[i].addEventListener("dragend", handleDragEnd, false);
         }
 
         for (let i = 0; i < doneListItems.length; i++) {
-            console.log(doneListItems[i]);
+            // console.log(doneListItems[i]);
             doneListItems[i].addEventListener("dragstart", handleDragStart, false);
-            doneListItems[i].addEventListener("dragend", handleDragEnd, false);
         }
-
-        return false;
     }
 
-    function handleDragStart(event) {
-        console.log(event.target);
-        return false;
+    function handleDragStart(evt) {
+        evt.dataTransfer.setData("text/plain", evt.target.id);
+        evt.dataTransfer.dropEffect = "move";
     }
 
-    function handleDragEnd(event) {
-        console.log(event.target);
-        return false;
+    function handleDrop(evt) {
+        evt.preventDefault();
+
+        var draggedElement = evt.dataTransfer.getData("text");
+        evt.target.appendChild(document.querySelector("#" + draggedElement));
+    }
+
+    function handleDragover(evt) {
+        evt.preventDefault();
     }
 })();
